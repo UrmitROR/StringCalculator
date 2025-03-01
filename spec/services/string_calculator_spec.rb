807c4f54d05a842f20f6d0215256e54ceca9b33c
Urmit <urmitprajapati1@gmail.com>
2025-03-01 15:21:14 +0530

    Modified spec file and wrriten code for case4 & case 5 & case 6 handles newlines as delimiters and supports custom delimiters and supports custom delimiters along with newlines

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -13,5 +13,17 @@ RSpec.describe StringCalculatorService do
     it "returns the sum of two comma-separated numbers" do
       expect(StringCalculatorService.add("1,5")).to eq(6)
     end
+
+    it "handles newlines as delimiters" do
+      expect(StringCalculatorService.add("1\n2,3")).to eq(6)
+    end
+
+    it "supports custom delimiters" do
+      expect(StringCalculatorService.add("//;\n1;2")).to eq(3)
+    end
+
+    it "supports custom delimiters along with newlines" do
+      expect(StringCalculatorService.add("//;\n1;2")).to eq(3)
+    end
   end
 end
