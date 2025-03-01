827154622427cbc727bebb5d2dcf4e02fdef0697
Urmit <urmitprajapati1@gmail.com>
2025-03-01 15:26:23 +0530

    Modified service file and wrriten code for case7: throws an exception for negative numbers

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -10,9 +10,15 @@ class StringCalculatorService
       delimiter = Regexp.escape(parts[0][2..])  # Extract the custom delimiter
       numbers = parts[1]
     end
-    
+
     # Split numbers using the delimiter
-    numbers.split(/#{delimiter}/).map(&:to_i).sum
+    num_list = numbers.split(/#{delimiter}/).map(&:to_i)
+
+    # Check for negatives
+    negatives = num_list.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(', ')}" if negatives.any?
+
+    num_list.sum
 
   end
 end
